dfee08292ed3da6bf8308b089e34e8e63abd4047
Porkyoot <85759819+porkyoot@users.noreply.github.com>
2025-12-31 04:03:27 +0100

    feat: Improve input field and slider styling, simplify date picker, and refactor logarithmic slider value display.

--- sortomatic/ui/components/atoms/inputs.py
+++ sortomatic/ui/components/atoms/inputs.py
@@ -6,20 +6,16 @@ def search_bar(placeholder: str = 'Search...', on_change: Optional[Callable] = N
     """
     An elegant text input with a search icon.
     """
-    inp = ui.input(placeholder=placeholder, on_change=on_change).classes(f'rounded-full px-4 py-1 text-sm bg-opacity-20')
+    inp = ui.input(placeholder=placeholder, on_change=on_change).classes('rounded-full px-4 py-1 text-sm bg-bg bg-opacity-20')
     inp.props('standout dense rounded item-aligned input-class="text-sm"')
     with inp.add_slot('prepend'):
         ui.icon('search').classes('text-xs text-muted')
     return inp
 
-def date_picker(on_change: Optional[Callable] = None) -> ui.input:
+def date_picker(label: str = 'Range', value: str = '', on_change: Optional[Callable] = None):
     """
-    A wrapper around the nicegui Date Picker to select a date range (start/end).
+    A thin wrapper around the NiceGUI date_input component for selecting a date range.
     """
-    with ui.input('Date Range').classes('text-sm') as date_input:
-        with date_input.add_slot('append'):
-            ui.icon('event').classes('cursor-pointer')
-            with ui.menu().props('no-parent-event') as menu:
-                # Range picker
-                ui.date(on_change=on_change).props('range')
-    return date_input
+    date = ui.date_input(label, value=value, range_input=True, on_change=on_change)
+    date.classes('w-60')
+    return date
